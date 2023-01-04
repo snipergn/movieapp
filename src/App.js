5b44316749f0ccdd82a3cfdd3c2a7fb4898abcc9
snipergn <andrei.banita@gmail.com>
2023-01-04 16:14:07 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      {/*
+      1.Frontend Page
+        -> Main Page 
+        -> Login && Register Page by User Data/ID
+        -> My list when you can add a move or remove from it.
+        -> Navbar list + Logo
+        -> Search Movie by genre, title or name author
+        -> Sign Out Header for Account
+      2. Backend
+        -> Crypt Data with Hash (Bycrypt)
+        -> GET user details from frontend
+        -> POST register details from frotnend
+        -> PUT -> When My list Add an object
+        -> DELETE -> When remove an item from my list of movies.
+
+      3. Database ?? (yet)
+      */}
     </div>
   );
 }
