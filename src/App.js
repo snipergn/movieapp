19f17923d6fe1a35c2f123b7f9f17b08cb4da0f5
snipergn <andrei.banita@gmail.com>
2023-02-23 18:57:08 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -8,48 +8,66 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      'movielatest': [],
-      'moviegenes': [],
+      'popularmovies': [],
+      'upcomingmovies': [],
+      'comedymovies': [],
+      "documentarymovies": []
 
     }
-    this.handleHeader = this.handleHeader.bind(this)
-
+    this.handleHeader = this.handleHeader.bind(this);
   }
   
 
   handleHeader = () => {
     const api = '399de7528a6f7ce137d42429f7513ad0'
-    const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api}&language=en-US&page=1`
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api}&language=en-US&page=1`
     fetch(url)
     .then(response => response.json())
     .then(data => {
-      let movieconcat = this.state.movielatest.concat(data)
+      let movieconcat = this.state.popularmovies.concat(data)
       let filtered = movieconcat.filter(filter => filter === data)
-      let objectkey = filtered[Object.keys(filtered)[0]]
       this.setState({
-        movielatest: filtered,
-        poster_path: objectkey
+        popularmovies: filtered,
       })
     })
-  }
-  handleMovieGenes = () => {
-    const api = '399de7528a6f7ce137d42429f7513ad0'
-    const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api}&language=en-US&page=1`
-    fetch(url)
+    const urlupcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${api}&language=en-US&page=1&region=US`
+    fetch(urlupcoming)
     .then(response => response.json())
     .then(data => {
-      let movieconcat = this.state.moviegenes.concat(data)
+      let movieconcat = this.state.upcomingmovies.concat(data)
       let filtered = movieconcat.filter(filter => filter === data)
       this.setState({
-        moviegenes: filtered
+        upcomingmovies: filtered
       })
     })
+    const getGen = `https://api.themoviedb.org/3/discover/movie?api_key=${api}&with_genres=35`
+    fetch(getGen)
+    .then(response => response.json())
+    .then(data => {
+      let movieconcat = this.state.comedymovies.concat(data)
+      let filtered = movieconcat.filter(filter => filter === data)
+      this.setState({
+        comedymovies: filtered
+      })
+      console.log(filtered)
+    })
+    const getDocumentary = `https://api.themoviedb.org/3/discover/movie?api_key=399de7528a6f7ce137d42429f7513ad0&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=10749&with_watch_monetization_types=flatrate`
+    fetch(getDocumentary)
+    .then(response => response.json())
+    .then(data => {
+      let movieconcat = this.state.documentarymovies.concat(data)
+      let filtered = movieconcat.filter(filter => filter === data)
+      this.setState({
+        documentarymovies: filtered
+      })
+      console.log(filtered)
+    })
   }
+  
 
   componentDidMount() {
     this.handleHeader();
-    this.handleMovieGenes()
-  }
+ }
 
 
 
@@ -63,7 +81,7 @@ class App extends React.Component {
         -> Login && Register Page by User Data/ID
         -> My list when you can add a move or remove from it.
         -> Navbar list + Logo
-        -> Search Movie by genre, title or name author
+        -> Search Movie by title
         -> Sign Out Header for Account
       2. Backend
         -> Crypt Data with Hash (Bycrypt)
@@ -75,8 +93,14 @@ class App extends React.Component {
       3. Database ?? (yet)
       */}
       <Navbar/>
-      <Header movielatest={this.state.movielatest}/>
-      <Section movielatest={this.state.movielatest}/>
+      <Header movielatest={this.state.popularmovies}/>
+      <Section 
+        movielatest={this.state.popularmovies} 
+        futureMovies={this.state.upcomingmovies} 
+        comedyMovies={this.state.comedymovies}
+        movieDocumentary={this.state.documentarymovies}
+
+      />
       <Footer/>
     </div>
     );
--- src/components/section/section.js
+++ src/components/section/section.js
@@ -1,15 +1,109 @@
 import React from "react";
 import "./section.css";
 
-const Section = ({ movielatest }) => {
+const Section = ({ movielatest, futureMovies, comedyMovies, movieDocumentary }) => {
   
   return (
     <div className="mt-2 ml-5 mr-5">
-    <h1 className="text-left mt-5">Latest movies</h1>
+    {/* LATEST MOVIES SECTION*/}
+    <h1 className="text-left mt-5">Popular</h1>
       {movielatest.map((item) => {
         let filterAPI = item.results.slice(0, 6)
         return (
           <div className="row row-cols-1 row-cols-md-3 g-4">
+            {filterAPI.map((item, index) => {
+              let results = item.overview.substr(0,60)
+              return (
+                  <div class="col-lg-2 col-md-4 col-sm-6 mt-3">
+                    <div class="card h-100">
+                      <img
+                        src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
+                        class="img-fluid"
+                        alt="img"
+                      />
+                      <div class="card-body">
+                        <h6 class="card-title">{item.title}</h6>
+                        <p class="card-text">
+                          {results}...
+                        </p>
+                        <button class="btn btn-primary" type="button">Add to Favorite</button>
+                      </div>
+                    </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+      {/* UPCOMING MOVIES SECTION*/}
+      <h1 className="text-left mt-5">Upcoming</h1>
+      {futureMovies.map((item) => {
+        let filterAPI = item.results.slice(0, 6)
+        return (
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {filterAPI.map((item, index) => {
+              let results = item.overview.substr(0,60)
+              return (
+                  <div class="col-lg-2 col-md-4 col-sm-6 mt-3">
+                    <div class="card h-100">
+                      <img
+                        src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
+                        class="img-fluid"
+                        alt="img"
+                      />
+                      <div class="card-body">
+                        <h6 class="card-title">{item.title}</h6>
+                        <p class="card-text">
+                          {results}...
+                        </p>
+                        <p>Release Date: <br/>
+                          <strong>{item.release_date}</strong>
+                        </p>
+                        <button class="btn btn-danger" type="button">Add to Favorite</button>
+                      </div>
+                    </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+      {/* COMEDY MOVIES SECTION*/}
+      <h1 className="text-left mt-5">Comedy</h1>
+      {comedyMovies.map((item) => {
+        let filterAPI = item.results.slice(0, 6)
+        return (
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {filterAPI.map((item, index) => {
+              let results = item.overview.substr(0,60)
+              return (
+                  <div class="col-lg-2 col-md-4 col-sm-6 mt-3">
+                    <div class="card h-100">
+                      <img
+                        src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
+                        class="img-fluid"
+                        alt="img"
+                      />
+                      <div class="card-body">
+                        <h6 class="card-title">{item.title}</h6>
+                        <p class="card-text">
+                          {results}...
+                        </p>
+                        <button class="btn btn-warning" type="button">Add to Favorite</button>
+                      </div>
+                    </div>
+                </div>
+              );
+            })}
+          </div>         
+        );
+      })}
+      {/* ROMANCE MOVIES SECTION*/}
+      <h1 className="text-left mt-5">Romance</h1>
+      {movieDocumentary.map((item) => {
+        let filterAPI = item.results.slice(0, 6)
+        return (
+          <div className="row row-cols-1 row-cols-md-3 g-4 mb-5">
             {filterAPI.map((item, index) => {
               let results = item.overview.substr(0,60)
               return (
